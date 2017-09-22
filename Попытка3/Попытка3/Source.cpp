2c00e4ea5456a861a24aff6b4772cebcb04750bd
olya757 <govorkova.t@yandex.ru>
2017-09-22 18:26:01 +0300

    22.09.2017 создала матрицу

--- Попытка3/Попытка3/Source.cpp
+++ Попытка3/Попытка3/Source.cpp
@@ -1,13 +1,34 @@
+/*Найти медиану графа, то есть найти такую вершину графа, 
+суммарное расстояние от которой до всех остальных вершин является наименьшим.*/
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+
 int main() {
-
-	cout << "Hello people" << endl;
-	if (3 > 2) {
-		cout << "True" << endl;
-
+	
+	/*Создание динамического массива*/
+	int n;
+	cout << "Введите количество вершин" << endl;
+	cin >> n;
+	int **Matr = new int*[n];
+	for (int i = 0; i < n; i++) {
+		Matr[i] = new int[n];
 	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			Matr[i][j] = 0;
+		}
+	}
+
+
+
+	/*Удаление массива из памяти*/
+	for (int i = 0; i < n; i++) {
+		delete[] Matr[i];
+	}
+	
 	return 0;
 }
